ede5847fe9bcdc3abf26a18544def2a262f9a168
jafarang <mohamadaminjafarizade@gmail.com>
2023-04-15 03:32:33 +0430

    add Some Buildings

--- ApProject/src/model/game_stuff/buildings/Mine.java
+++ ApProject/src/model/game_stuff/buildings/Mine.java
@@ -1,11 +1,12 @@
 package model.game_stuff.buildings;
 
+import model.game_stuff.Building;
 import model.game_stuff.Food;
 import model.game_stuff.Good;
 import model.game_stuff.buildings.enums.FarmType;
 import model.game_stuff.buildings.enums.MineTypes;
 
-public class Mine {
+public class Mine extends Building {
     private Good goodToBeGenerated;
     private MineTypes type;
 
