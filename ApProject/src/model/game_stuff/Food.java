e0c134baa12a510ee407b199c0f8bc53fd2bcb2b
mirza <momirzadi2004@gmail.com>
2023-04-14 07:11:54 -0700

    adding basics

--- ApProject/src/model/game_stuff/Food.java
+++ ApProject/src/model/game_stuff/Food.java
@@ -1,4 +1,14 @@
 package model.game_stuff;
 
 public enum Food {
+    bread("bread"),
+    meat("meat"),
+    milk("milk"),
+    rice("rice");
+
+    private String name;
+
+    Food(String name) {
+        this.name = name;
+    }
 }
