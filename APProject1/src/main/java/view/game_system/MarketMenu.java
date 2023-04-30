e3794c1a4f91eda47d3a06afdb6040b25f18ad07
alirezamafa <122224802+alirezamafa@users.noreply.github.com>
2023-05-01 00:44:23 +0430

    add MarketMenu.java

--- APProject1/src/main/java/view/game_system/MarketMenu.java
+++ APProject1/src/main/java/view/game_system/MarketMenu.java
@@ -5,9 +5,17 @@ import view.game_system.messages.GameSwitcherMessages;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
-public class MarketMenu {
+public class MarketMenu extends ViewUtils {
     public static GameSwitcherMessages run(Scanner scanner){
-        return null;
+        String command ;
+        Matcher matcher;
+        while (true){
+            command=scanner.nextLine().trim();
+            if(command.matches("exit")){
+                return GameSwitcherMessages.GAME;
+            }
+            else if( (matcher=TradeCommands.get))
+        }
     }
 
     private static void showPriceList(Matcher matcher){
