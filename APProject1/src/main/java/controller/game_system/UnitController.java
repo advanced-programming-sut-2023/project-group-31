34fb378f07c5dac45d4d4d1b1d1147a1c186714f
mirza <momirzadi2004@gmail.com>
2023-05-12 15:13:34 -0700

    doing whit start game controller

--- APProject1/src/main/java/controller/game_system/UnitController.java
+++ APProject1/src/main/java/controller/game_system/UnitController.java
@@ -1,10 +1,23 @@
 package controller.game_system;
 
 import controller.ControllerUtils;
+import model.game_stuff.Person;
 import model.game_stuff.Troop;
 
 import java.util.ArrayList;
 
 public class UnitController extends ControllerUtils {
     static private ArrayList<Troop> troops;
+
+    public static UnitMessages selectUnit(int x,int y){
+        troops=new ArrayList<Troop>();
+        for(Person person:currentGame.getMap().getBlock(x,y).getPeople()){
+            if(person instanceof Troop){
+                troops.add((Troop)person);
+            }
+        }
+        if(troops.size()==0){
+            return U;
+        }
+    }
 }
