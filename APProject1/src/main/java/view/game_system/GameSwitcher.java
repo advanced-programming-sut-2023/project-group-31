d67f970716c269de62d0664003472e201ffbfaf2
mirza <momirzadi2004@gmail.com>
2023-06-05 07:15:12 -0700

    start game

--- APProject1/src/main/java/view/game_system/GameSwitcher.java
+++ APProject1/src/main/java/view/game_system/GameSwitcher.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import view.enums.Menus;
 import view.user_system.StrongHoldCrusaderGame;
-
 import java.io.IOException;
 import java.net.URL;
 
--- APProject1/src/main/java/view/game_system/pages/StartGame.java
+++ APProject1/src/main/java/view/game_system/pages/StartGame.java
@@ -1,10 +1,14 @@
 package view.game_system.pages;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -12,6 +16,7 @@ import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import model.DataBase;
 import model.User;
+
 import java.io.IOException;
 
 public class StartGame {
@@ -25,35 +30,37 @@ public class StartGame {
 
     @FXML
     private VBox playersVbox;
+    @FXML
+    private ScrollPane playerScroll;
 
     @FXML
-    public void initialize() {
-        checkScroll(playersVbox);
+    public void initialize() throws IOException{
         setPlayersList();
     }
 
-    private void checkScroll(VBox playersVbox) {
-        playersVbox.setOnScroll((ScrollEvent event) -> {
-            System.out.println(event.getDeltaY());
-        });
-    }
+
+
 
     private void setPlayersList() {
-        createPlayerRow();
+//        for(int i=0;i<playersVbox.getChildren().size();i++){
+//            playersVbox.getChildren().remove(i);
+//            i--;
+//        }
         for (int i = playersStart; i < playersStart + playersCount; i++) {
-
-            //((ImageView)(hBox.getChildren().get(0))).setImage(new Image(DataBase.getDataBase().getUsers().get(i).getUrl()));
-            //((Label)(hBox.getChildren().get(1))).setText(DataBase.getDataBase().getUsers().get(i).getUrl());
+            if (i >= DataBase.getDataBase().getUsers().size()) {
+                return;
+            }
+            playersVbox.getChildren().add(createPlayerRow(i));
         }
     }
 
-    private HBox createPlayerRow(){
-        //User user = DataBase.getDataBase().getUsers().get(index);
+    private HBox createPlayerRow(int index) {
+        User user = DataBase.getDataBase().getUsers().get(index);
         HBox hBox = new HBox();
         hBox.setStyle("-fx-start-margin: 20");
-//        ImageView imageView = new ImageView("new Image(user.getUrl())");
-//        imageView.setFitHeight(43.0);
-//        imageView.setFitWidth(58.0);
+        ImageView imageView = new ImageView(new Image(user.getUrl()));
+        imageView.setFitHeight(43.0);
+        imageView.setFitWidth(58.0);
         Label label1 = new Label();
         label1.setAlignment(Pos.CENTER);
         label1.setPrefHeight(57.0);
@@ -61,37 +68,36 @@ public class StartGame {
         label1.setTextFill(Paint.valueOf("#c91515"));
         label1.setFont(Font.font("System Bold"));
         label1.setStyle("-fx-font-size: 24");
-        label1.setText("user.getUsername()");
+        label1.setText(user.getUsername());
         Label label2 = new Label();
         label2.setAlignment(Pos.CENTER);
         label2.setPrefHeight(57.0);
         label2.setPrefWidth(150.0);
         label2.setTextFill(Paint.valueOf("#c91515"));
         label2.setFont(Font.font("System Bold"));
-        label2.setText("user.getNickname()");
+        label2.setText(user.getNickname());
         label2.setStyle("-fx-font-size: 22");
-//        hBox.getChildren().add(imageView);
+        Button button = new Button();
+        button.setMnemonicParsing(false);
+        button.setPrefHeight(52);
+        button.setPrefWidth(113);
+        button.setText("add");
+        button.setId(user.getUsername());
+        hBox.getChildren().add(imageView);
         hBox.getChildren().add(label1);
         hBox.getChildren().add(label2);
+        hBox.getChildren().add(button);
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+            }
+        });
         return hBox;
     }
-//                        <HBox>
-//                        <ImageView id="boxImage" fitHeight="43.0" fitWidth="58.0">
-//                            <Image url="@../../Media/Avatars/avatar1.png"/>
-//
-//                        </ImageView>
-//
-//                        <Label alignment="CENTER" prefHeight="57.0" prefWidth="150.0" text="maamad" textFill="#c91515">
-//                            <font>
-//                                <Font name="System Bold" size="24.0"/>
-//                            </font>
-//                        </Label>
-//                        <Label alignment="CENTER" prefHeight="54.0" prefWidth="206.0" text="mmirzadi"
-//    textFill="#c91515">
-//                            <font>
-//                                <Font name="System Bold" size="20.0"/>
-//                            </font>
-//                        </Label>
-//                        <Button mnemonicParsing="false" prefHeight="52.0" prefWidth="113.0" text="add"/>
-//                    </HBox>
+
+    public void checkScroll(ScrollEvent event) {
+            System.out.println(event.getDeltaY());
+
+    }
 }
